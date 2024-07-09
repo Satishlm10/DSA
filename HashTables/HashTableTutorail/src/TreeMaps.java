568f407c53f5672cc88b1970f9843d5b63f4beb1
Satish Maharjan <Satish.maharjan77@gmail.com>
2024-07-10 00:46:16 +0545

    Hash Table Data Structure Practice

---
+++ HashTables/HashTableTutorail/src/TreeMaps.java
@@ -0,0 +1,22 @@
+import java.util.TreeMap;
+
+public class TreeMaps {
+
+    public void treeMapExercise(){
+        TreeMap<Integer,String> treeMaps = new TreeMap<>();
+
+
+        // tree map is a sorted hash table/ map
+        treeMaps.put(100,"abcd");
+        treeMaps.put(5,"a");
+        treeMaps.put(1,"b");
+        treeMaps.put(3,"c");
+        treeMaps.put(101,"d");
+
+        System.out.println("TreeMap Example"+treeMaps);
+        System.out.println();
+
+
+
+    }
+}
